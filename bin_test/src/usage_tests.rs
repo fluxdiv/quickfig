ace6e1141237795f1b2f40d535f6b46654c5c38e
fluxdiv <156196590+fluxdiv@users.noreply.github.com>
2025-08-11 14:58:31 -0400

    TESTS/BUILD: Add some tests for unsigned/signed ints & update serde_json to include arbitrary_precision feature for u128/i128 serialization

--- bin_test/src/usage_tests.rs
+++ bin_test/src/usage_tests.rs
@@ -30,24 +30,25 @@ pub enum TestEnum {
     Char,
     #[must_be(u8)]
     U8,
-    // #[must_be(u16)]
-    // U16,
-    // #[must_be(u32)]
-    // U32,
-    // #[must_be(u64)]
-    // U64,
-    // #[must_be(u128)]
-    // U128,
-    // #[must_be(i8)]
-    // I8,
-    // #[must_be(i16)]
-    // I16,
-    // #[must_be(i32)]
-    // I32,
-    // #[must_be(i64)]
-    // I64,
-    // #[must_be(i128)]
-    // I128,
+
+    #[must_be(u16)]
+    U16,
+    #[must_be(u32)]
+    U32,
+    #[must_be(u64)]
+    U64,
+    #[must_be(u128)]
+    U128,
+    #[must_be(i8)]
+    I8,
+    #[must_be(i16)]
+    I16,
+    #[must_be(i32)]
+    I32,
+    #[must_be(i64)]
+    I64,
+    #[must_be(i128)]
+    I128,
     // #[must_be(bool)]
     // Bool,
     // #[must_be(f32)]
@@ -156,7 +157,7 @@ mod must_be_json {
     }
 
     // ---------------------------------------------------------------
-    // ------------ u8
+    // ------------ u8, u16, u32, u64, u128
     #[test]
     fn must_be_u8_ok() {
         let mut testconfig = TestFile::new(TFT::JSON).unwrap();
@@ -189,5 +190,295 @@ mod must_be_json {
         assert!(vals.is_none());
     }
 
+    #[test]
+    fn must_be_u16_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U16", 255)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U16"));
+        let vals = config.get(TestEnum::U16);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_u16()
+                .map(|u16_val| u16_val == 255)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_u16_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U16", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U16"));
+        let vals = config.get(TestEnum::U16);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_u32_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U32", 255)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U32"));
+        let vals = config.get(TestEnum::U32);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_u32()
+                .map(|u32_val| u32_val == 255)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_u32_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U32", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U32"));
+        let vals = config.get(TestEnum::U32);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_u64_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U64", 255)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U64"));
+        let vals = config.get(TestEnum::U64);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_u64()
+                .map(|u64_val| u64_val == 255)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_u64_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U64", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U64"));
+        let vals = config.get(TestEnum::U64);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_u128_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U128", 255)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U128"));
+        let vals = config.get(TestEnum::U128);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_u128()
+                .map(|u128_val| u128_val == 255)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_u128_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("U128", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("U128"));
+        let vals = config.get(TestEnum::U128);
+        assert!(vals.is_none());
+    }
+
+    // ---------------------------------------------------------------
+    // ------------ i8, i16, i32, i64, i128
+    #[test]
+    fn must_be_i8_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I8", -128)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I8"));
+        let vals = config.get(TestEnum::I8);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_i8()
+                .map(|i8_val| i8_val == -128)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_i8_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I8", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I8"));
+        let vals = config.get(TestEnum::I8);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_i16_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I16", i16::MIN)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I16"));
+        let vals = config.get(TestEnum::I16);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_i16()
+                .map(|i16_val| i16_val == i16::MIN)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_i16_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I16", (i16::MIN as i32) - 1)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I16"));
+        let vals = config.get(TestEnum::I16);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_i32_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I32", i32::MIN)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I32"));
+        let vals = config.get(TestEnum::I32);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_i32()
+                .map(|i32_val| i32_val == i32::MIN)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_i32_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I32", (i32::MIN as i64) - 1)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I32"));
+        let vals = config.get(TestEnum::I32);
+        assert!(vals.is_none());
+    }
+
+    #[test]
+    fn must_be_i64_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I64", i64::MIN)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I64"));
+        let vals = config.get(TestEnum::I64);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_i64()
+                .map(|i64_val| i64_val == i64::MIN)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_i64_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I64", (i64::MIN as i128) - 1)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I64"));
+        let vals = config.get(TestEnum::I64);
+        assert!(vals.is_none());
+    }
+
+    // ERR
+    #[test]
+    fn must_be_i128_ok() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I128", i128::MIN)).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I128"));
+        let vals = config.get(TestEnum::I128);
+        assert!(vals.is_some());
+        let vals = vals.unwrap();
+        assert!(vals.len() == 1);
+        let inner = vals.iter().find(|v| {
+            v.get_i128()
+                .map(|i128_val| i128_val == i128::MIN)
+                .is_some()
+        });
+        assert!(inner.is_some());
+    }
+
+    #[test]
+    fn must_be_i128_err() {
+        let mut testconfig = TestFile::new(TFT::JSON).unwrap();
+        testconfig.add_entry(("I128", "foo")).unwrap();
+        let config = Config::<JSON>::open(testconfig.get_path()).unwrap();
+        testconfig.delete();
+
+        assert!(config.has_key("I128"));
+        let vals = config.get(TestEnum::I128);
+        assert!(vals.is_none());
+    }
 }
 
